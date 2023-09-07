b144c917d4de367032b0c8f5379580ac9d3c1fb6
Marat Garifulin <marat.garifulin160@gmail.com>
2023-09-07 22:06:13 +0400

    Fluent db

---
+++ FluentDb/FluentDb/Data/DbContexts/UserDbContext.cs
@@ -0,0 +1,45 @@
+﻿using FluentDb.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace FluentDb.Data.DbContexts
+{
+    public class UserDbContext : DbContext
+    {
+        public 
+            DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            ConfigurationBuilder builder = new();
+            builder.AddJsonFile("appsettings.json");
+
+
+            IConfigurationRoot config = builder.Build();
+            var connectionString = config.GetConnectionString("UserDb");
+
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+
+                entity.HasIndex(u => u.Username).IsUnique();
+
+                entity.Property(u => u.Name).HasMaxLength(50);
+
+                entity.Property(u => u.Surname).HasMaxLength(50);
+
+                entity.Property(u => u.Password).HasMaxLength(70);
+
+                entity.Property(u => u.Email).HasMaxLength(256).IsRequired();
+
+                entity.Property(e => e.Age).IsRequired();
+            }
+            );
+        }
+    }
+}
---
+++ FluentDb/FluentDb/Data/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace FluentDb.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
